4a485e058108168f12180bb86d0276cb57496941
Derrick Mei <derrick.mei.168@gmail.com>
2020-03-06 00:50:40 -0600

    binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,3 +9,24 @@ def linear_search(arr, target):
     return -1   # not found
 
 
+# STRETCH: write an iterative implementation of Binary Search
+def binary_search(arr, target):
+
+    if len(arr) == 0:
+        return -1  # array empty
+
+    low = 0
+    high = len(arr)-1
+
+    # TO-DO: add missing code
+    middle = (low + high) // 2
+    if arr[middle] == target:
+        return target
+    elif target < arr[middle]:
+        high = middle - 1
+    elif target > arr[middle]:
+        low = middle + 1
+
+    return -1  # not found
+
+
